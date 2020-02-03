9c61c466b1a16954047ec355f6ba22de2c1d4af5
[Students] Emilija Zabina <81DEZabina@rvt.lv>
2020-02-03 14:05:42 +0200

    add file read and write

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from file_proc import read_file, write_file
 
 app = Flask(__name__)
 
@@ -26,6 +27,30 @@ def params():
 @app.route('/post', methods = ['POST'])
 def post ():
   return request.get_json()
+
+@app.route('/read_from_file')
+def read_from_file():
+  content = read_file
+  return content
+
+@app.route ('/write_into_file')
+def write_into_file():
+  request_type = request.content_type
+  if (request_type == "application/json"):
+    contentJSON = request.get_json
+    write_file(contentJSON)
+    return contentJSON
+  else:
+    return f"Request type {request_type} not supported!"
+
+@aoo.route('/file', methods = ['POST', 'GET'])
+def file():
+  if request.method == 'GET':
+    return read_from_file()
+  if request.method == 'POST':
+    return write_into_file()
+
+
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 5211, threaded = True, debug = True)
